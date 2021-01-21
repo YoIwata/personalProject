2ae7d4fec5ce989eec86ac720d1b7fd612c05737
Yoya_Iwata <thunroq7@gmail.com>
2021-01-22 00:00:11 +0900

    Postなど修正

--- src/main/java/com/example/personalProject/controller/FormController.java
+++ src/main/java/com/example/personalProject/controller/FormController.java
@@ -3,6 +3,8 @@ package com.example.personalProject.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,17 +27,21 @@ public class FormController {
 		return list;
 	}
 
-	public void doPost(Form form) {
+	@RequestMapping(value = "/result", method=RequestMethod.POST)
+	public Form doPost(@RequestBody Form form) {
 		formDao.insertForm(form);
 
+		return form;
 	}
 
-	public void doPut(Form form) {
+	@RequestMapping(value = "/index/{id}", method=RequestMethod.PUT)
+	public void doPut(@PathVariable("id") Integer id, @RequestBody Form form) {
 		formDao.updateForm(form);
 
 	}
 
-	public void doDelete(Integer id) {
+	@RequestMapping(value = "/index/{id}", method=RequestMethod.DELETE)
+	public void doDelete(@PathVariable("id") Integer id) {
 		formDao.deleteForm(id);
 
 	}
